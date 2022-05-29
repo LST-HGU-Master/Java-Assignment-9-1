d14a404fe9e3e95596b4647ca333fe9e268a4b01
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-29 21:09:59 +0900

    Update Prog91.java

--- src/main/java/Prog91.java
+++ src/main/java/Prog91.java
@@ -1,11 +1,10 @@
 public class Prog91 {
 
-	public static void main(String[] args){
-		InvisibleHero sh = new InvisibleHero(); //インスタンスの変数名は指定していないので、任意でOK
-		sh.getInvisible(); //見えなくなる
-		sh.getVisible(); //姿を現す
-		sh.run(); //逃げる
-	}
+    public static void main(String[] args){
+        InvisibleHero sh = new InvisibleHero(); //インスタンスの変数名は指定していないので、任意でOK
+        sh.getInvisible(); //見えなくなる
+        sh.getVisible(); //姿を現す
+        sh.run(); //逃げる
+    }
 
 }
-
