6c1f8483c1889bd6bc82c8030b1c37ce3de49001
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-04 16:18:26 +0900

    Update Prog91Test.java

--- src/test/java/Prog91Test.java
+++ src/test/java/Prog91Test.java
@@ -1,7 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-
+/**
+ * @version (20220604)
+ **/
 public class Prog91Test {
 
     @Test
@@ -10,19 +13,26 @@ public class Prog91Test {
         PrintStream originalOut = System.out;
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(bos));
-
         StandardInputStream in = new StandardInputStream();
         System.setIn(in);
-
+        
         // action
         // in.inputln("2"); // 標準入力をテストする場合
         // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         Prog91.main(null);
+        
+        System.setOut(originalOut);        
 
         // assertion
-        assertEquals("Hello world!\n", bos.toString());
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        String[] prints = bos.toString().split("\r\n|\n", -1);
+        try {
+            assertEquals("勇者工太は姿を消した！", prints[0]);
+            assertEquals("勇者工太は姿を現した！", prints[1]);
+            assertEquals("工太は、逃げ出した！", prints[2]);
+            assertEquals("最終HPは100でした", prints[3]);
+        } catch (ArrayIndexOutOfBoundsException excpt) {
+            fail("Prog81.main()のprint出力に改行が4つ分ありません!");
+        }
+        assertEquals(5,prints.length,"改行数が５つ以上あります!"); // このif文に到達した時点で prints.lengthは５以上になっている
     }
 }
