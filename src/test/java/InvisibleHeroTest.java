90642f7cc5a03be4df3cdcbb851e1b027a1dcacd
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-04 16:18:39 +0900

    Update InvisibleHeroTest.java

--- src/test/java/InvisibleHeroTest.java
+++ src/test/java/InvisibleHeroTest.java
@@ -3,7 +3,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-
+/**
+ * @version (20220604)
+ * 注意）InvisibleHero.isVisible、
+ *      InvisibleHero.getInvisible()、
+ *      InvisibleHero.getVisible()の３つが定義
+ *      及びInvisibleHeroがHeroを継承するまではエラーとなる。
+ *      
+ *      print命令の末尾にある感嘆符は全角文字とする
+ *      改行数を厳密に評価している
+ **/
 public class InvisibleHeroTest {
 
     @Test
@@ -13,7 +22,7 @@ public class InvisibleHeroTest {
         InvisibleHero h = new InvisibleHero();
 
         // assertion
-        assertTrue(h instanceof Hero);
+        assertTrue(h instanceof Hero,"InvisibleHeroはHeroを継承していません!");
     }
 
     @Test
@@ -23,19 +32,27 @@ public class InvisibleHeroTest {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(bos));
 
-        // action
-        InvisibleHero h = new InvisibleHero();
-        // assertion
-        assertTrue(h.isVisible);
+        try {
+            // action
+            InvisibleHero h = new InvisibleHero();
+            // assertion
+            assertTrue(h.isVisible,"newで作られたばかりのInvisibleHeroインスタンスのisVisibleがfalseです!");
 
-        // action
-        h.getInvisible();
-        // assertion
-        assertFalse(h.isVisible);
-        assertEquals("勇者工太は姿を消した!\n", bos.toString());
+            // action
+            h.getInvisible();
+            // undo the binding in System
+            System.setOut(originalOut);
+            
+            String[] prints = bos.toString().split("\r\n|\n", -1); // 値が空の部分も切り出す
+            // assertion
+            assertFalse(h.isVisible,"getInvisible()を呼び出してもisVisibleがfalseになりません!");
+            assertEquals("勇者工太は姿を消した！", prints[0].toString(),"getInvisible()を呼び出した場合のprint出力が実行例と異なります!");
+            assertEquals(2,prints.length,"改行が２つ以上あります!");
+        } catch (AssertionError err) {
+            System.setOut(originalOut);
+            throw err;
+        }
 
-        // undo the binding in System
-        System.setOut(originalOut);
     }
 
     @Test
@@ -49,12 +66,14 @@ public class InvisibleHeroTest {
         InvisibleHero h = new InvisibleHero();
         h.isVisible = false;
         h.getVisible();
-
-        // assertion
-        assertTrue(h.isVisible);
-        assertEquals("勇者工太は姿を現した!\n", bos.toString());
-
         // undo the binding in System
         System.setOut(originalOut);
+        
+        String[] prints = bos.toString().split("\r\n|\n", -1); // 値が空の部分も切り出す
+
+        // assertion
+        assertTrue(h.isVisible,"getVisible()を呼び出してもisVisibleがtrueになりません!");
+        assertEquals("勇者工太は姿を現した！", prints[0].toString(),"getVisible()を呼び出した場合のprint出力が実行例と異なります!");
+        assertEquals(2,prints.length,"改行が２つ以上あります!");
     }
 }
